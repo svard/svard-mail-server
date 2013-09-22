c6fe4d883ade8cadeefd0f214596029ceccb42b1
Kristofer Svärd <kristofer.svard@gmail.com>
2013-09-22 17:57:49 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject svard-mail-server "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [compojure "1.1.5" :exclusions [ring/ring-core org.clojure/core.incubator]]
+                 [com.cemerick/friend "0.1.5"]
+                 [org.clojure/data.json "0.2.3"]
+                 [enlive "1.1.4"]]
+  :plugins [[lein-ring "0.8.7"]]
+  :ring {:handler svard-mail-server.handler/app}
+  :profiles
+  {:dev {:dependencies [[ring-mock "0.1.5"]]}})
---
+++ src/svard_mail_server/handler.clj
@@ -0,0 +1,15 @@
+(ns svard-mail-server.handler
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]
+            [cemerick.friend :as friend]
+            (cemerick.friend [credentials :as creds]
+                             [workflows :as workflows])))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
